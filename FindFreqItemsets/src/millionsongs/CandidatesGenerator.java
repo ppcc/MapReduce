3bfd8b2bb676d4090b47bc4f38da5bf9fb5e5ad1
ppcc <cao.pei@husky.neu.edu>
2014-12-06 19:35:26 -0500

    Reformat

--- FindFreqItemsets/src/millionsongs/CandidatesGenerator.java
+++ FindFreqItemsets/src/millionsongs/CandidatesGenerator.java
@@ -5,201 +5,229 @@ import java.io.IOException;
 import java.util.*;
 
 /**
- * Generate k frequent itemsets from k-itemset candidates' files.
+ * Generate k frequent itemsets from k-itemset candidates' files.<br>
  * Created by peilicao on 12/01/14.
  */
 public class CandidatesGenerator {
 
-    FileReadWriteUtil utils = new FileReadWriteUtil();
+	FileReadWriteUtil utils = new FileReadWriteUtil();
 
-    /**
-     * Check if two lists are qualified to be used to generate candidate.<br>
-     * Return return true if first[0..-2] are equals to second[0...-2],
-     * otherwise return false.
-     * <p>
-     * The two list must already be ordered ascending.
-     * 
-     * @param first - a list of <code>songid</code>
-     * @param second - a list of <code>songid</code>
-     * @return 
-     */
-    public boolean isQualified(List<Integer> first, List<Integer> second) {
-        int idx1 = 0;
-        int idx2 = 0;
-        while (idx1 < first.size() - 1 && idx2 < second.size() - 1) {
-            if (!first.get(idx1).equals(second.get(idx2)))
-                return false;
-            idx1++;
-            idx2++;
-        }
-        return true;
-    }
+	/**
+	 * Check if two lists are qualified to be used to generate candidate.<br>
+	 * Return return true if first[0..-2] are equals to second[0...-2],
+	 * otherwise return false.
+	 * <p>
+	 * The two list must already be ordered ascending.
+	 * 
+	 * @param first
+	 *            - a list of <code>songid</code>
+	 * @param second
+	 *            - a list of <code>songid</code>
+	 * @return
+	 */
+	public boolean isQualified(List<Integer> first, List<Integer> second) {
+		int idx1 = 0;
+		int idx2 = 0;
+		while (idx1 < first.size() - 1 && idx2 < second.size() - 1) {
+			if (!first.get(idx1).equals(second.get(idx2)))
+				return false;
+			idx1++;
+			idx2++;
+		}
+		return true;
+	}
 
-    /**
-     * Generate Candidates_2 by self join one frequent items. One frequent items are loaded from
-     * folder name provided.
-     * @param folderName - Folder contains one frequent items
-     * @param conf - job configuration for loading files from HDFS
-     * @return List<<>> Candidates_2
-     * @throws IOException
-     */
-    public List<List<Integer>> generateCandidates2(String folderName,Configuration conf) throws Exception {
-        List<Integer> oneFreqItems = loadOneFreqItems(folderName,conf);
-        List<List<Integer>> candidates = generateCandidates2(oneFreqItems);
-        return candidates;
-    }
+	/**
+	 * Generate Candidates_2 by self join one frequent items. One frequent items
+	 * are loaded from folder name provided.
+	 * 
+	 * @param folderName
+	 *            - Folder contains one frequent items
+	 * @param conf
+	 *            - job configuration for loading files from HDFS
+	 * @return List<<>> Candidates_2
+	 * @throws IOException
+	 */
+	public List<List<Integer>> generateCandidates2(String folderName,
+			Configuration conf) throws Exception {
+		List<Integer> oneFreqItems = loadOneFreqItems(folderName, conf);
+		List<List<Integer>> candidates = generateCandidates2(oneFreqItems);
+		return candidates;
+	}
 
-    /**
-     * Generate Candidates_2 by self join one frequent items. One frequent items are provided through parameters
-     * @param oneFreqItems - List of one frequent items
-     * @return a list of candidates2
-     */
-    public List<List<Integer>> generateCandidates2(List<Integer> oneFreqItems) {
+	/**
+	 * Generate Candidates_2 by self join one frequent items. One frequent items
+	 * are provided through parameters
+	 * 
+	 * @param oneFreqItems
+	 *            - List of one frequent items
+	 * @return a list of candidates2
+	 */
+	public List<List<Integer>> generateCandidates2(List<Integer> oneFreqItems) {
 
-        List<List<Integer>> candidates = new ArrayList<List<Integer>>();
-        int size = oneFreqItems.size();
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                List<Integer> list = new ArrayList<Integer>();
-                list.add(oneFreqItems.get(i));
-                list.add(oneFreqItems.get(j));
-                candidates.add(list);
-            }
-        }
-        return candidates;
-    }
+		List<List<Integer>> candidates = new ArrayList<List<Integer>>();
+		int size = oneFreqItems.size();
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				List<Integer> list = new ArrayList<Integer>();
+				list.add(oneFreqItems.get(i));
+				list.add(oneFreqItems.get(j));
+				candidates.add(list);
+			}
+		}
+		return candidates;
+	}
 
-    /**
-     * Generate candidates_k by doing (k-1) frequent itemsets self join.<br>
-     * (k-1) frequent itemsets is loaded from file.
-     * @param folderName Folder L_(k-1) which contains frequent itemsets with k-1 items
-     * @param conf job configuration for loading files from HDFS
-     * @return a list of candidates_k
-     * @throws IOException
-     */
-    public List<List<Integer>> generateCandidates(String folderName, Configuration conf) throws Exception {
-        List<List<Integer>> freqItemsets = loadFreqItems(folderName,conf);
-        List<List<Integer>> candidates = generateCandidates(freqItemsets);
-        return candidates;
-    }
+	/**
+	 * Generate candidates_k by doing (k-1) frequent itemsets self join.<br>
+	 * (k-1) frequent itemsets is loaded from file.
+	 * 
+	 * @param folderName
+	 *            Folder L_(k-1) which contains frequent itemsets with k-1 items
+	 * @param conf
+	 *            job configuration for loading files from HDFS
+	 * @return a list of candidates_k
+	 * @throws IOException
+	 */
+	public List<List<Integer>> generateCandidates(String folderName,
+			Configuration conf) throws Exception {
+		List<List<Integer>> freqItemsets = loadFreqItems(folderName, conf);
+		List<List<Integer>> candidates = generateCandidates(freqItemsets);
+		return candidates;
+	}
 
-    /**
-     * Generate candidates_k by doing (k-1) frequent itemsets self join.<br>
-     * (k-1) frequent itemsets is given by parameters.
-     * @param freqItemsets List of frequent itemsets with k-1 items
-     * @return a list of candidates_k
-     * @throws IOException
-     */
-    public List<List<Integer>> generateCandidates(List<List<Integer>> freqItemsets) throws IOException {
-        List<List<Integer>> candidates = selfJoinFreqItemsets(freqItemsets);
-        System.out.format("Candidates original size:%d\n", candidates.size());
-        prune(freqItemsets, candidates);
-        System.out.format("Candidates after pruned size:%d\n", candidates.size());
-        return candidates;
-    }
+	/**
+	 * Generate candidates_k by doing (k-1) frequent itemsets self join.<br>
+	 * (k-1) frequent itemsets is given by parameters.
+	 * 
+	 * @param freqItemsets
+	 *            List of frequent itemsets with k-1 items
+	 * @return a list of candidates_k
+	 * @throws IOException
+	 */
+	public List<List<Integer>> generateCandidates(
+			List<List<Integer>> freqItemsets) throws IOException {
+		List<List<Integer>> candidates = selfJoinFreqItemsets(freqItemsets);
+		System.out.format("Candidates original size:%d\n", candidates.size());
+		prune(freqItemsets, candidates);
+		System.out.format("Candidates after pruned size:%d\n",
+				candidates.size());
+		return candidates;
+	}
 
-    /**
-     * Generate all subsets which is only 1 item less than fatherSet
-     * @param fatherSet
-     * @return
-     */
-    public List<List<Integer>> generateAllSubsets(List<Integer> fatherSet) {
-        List<List<Integer>> subsets = new ArrayList<List<Integer>>();
-        for (int i = 0; i < fatherSet.size(); i++) {
-            List<Integer> subset = new ArrayList<Integer>();
-            for (int j = 0; j < fatherSet.size(); j++) {
-                if (j != i)
-                    subset.add(fatherSet.get(j));
-            }
-            subsets.add(subset);
-        }
-        return subsets;
-    }
+	/**
+	 * Generate all subsets which is only 1 item less than fatherSet
+	 * 
+	 * @param fatherSet
+	 * @return
+	 */
+	public List<List<Integer>> generateAllSubsets(List<Integer> fatherSet) {
+		List<List<Integer>> subsets = new ArrayList<List<Integer>>();
+		for (int i = 0; i < fatherSet.size(); i++) {
+			List<Integer> subset = new ArrayList<Integer>();
+			for (int j = 0; j < fatherSet.size(); j++) {
+				if (j != i)
+					subset.add(fatherSet.get(j));
+			}
+			subsets.add(subset);
+		}
+		return subsets;
+	}
 
-    /**
-     * Prune candidates. Remove the certain candidate if and only if it contains subset which is not in L_k-1
-     * @param freqItemsets L_k-1, frequent itemsets with k-1 items
-     * @param candidates C_k, frequent itemset candidates with k items
-     */
-    private void prune(List<List<Integer>> freqItemsets, List<List<Integer>> candidates) {
-        Iterator<List<Integer>> itr = candidates.iterator();
-        List<Integer> candidate;
-        List<List<Integer>> subsets;
-        while (itr.hasNext()) {
-            candidate = itr.next();
-            subsets = generateAllSubsets(candidate);
-            //Check if all subsets of candidate is in freqItemsets
-            if (!freqItemsets.containsAll(subsets)) {
-                itr.remove();
-            }
-        }
-    }
+	/**
+	 * Prune candidates. Remove the certain candidate if and only if it contains
+	 * subset which is not in L_k-1
+	 * 
+	 * @param freqItemsets
+	 *            L_k-1, frequent itemsets with k-1 items
+	 * @param candidates
+	 *            C_k, frequent itemset candidates with k items
+	 */
+	private void prune(List<List<Integer>> freqItemsets,
+			List<List<Integer>> candidates) {
+		Iterator<List<Integer>> itr = candidates.iterator();
+		List<Integer> candidate;
+		List<List<Integer>> subsets;
+		while (itr.hasNext()) {
+			candidate = itr.next();
+			subsets = generateAllSubsets(candidate);
+			// Check if all subsets of candidate is in freqItemsets
+			if (!freqItemsets.containsAll(subsets)) {
+				itr.remove();
+			}
+		}
+	}
 
-    /**
-     * Self join a list of itemsets.
-     * Only join two list if and only if their items are equals except for last one
-     * @param freqItemsets
-     * @return
-     */
-    private List<List<Integer>> selfJoinFreqItemsets(List<List<Integer>> freqItemsets) {
-        List<List<Integer>> candidates = new ArrayList<List<Integer>>();
-        int size = freqItemsets.size();
-        List<Integer> first;
-        List<Integer> second;
-        List<Integer> cand;
-        for (int i = 0; i < size; i++) {
-            first = freqItemsets.get(i);
-            for (int j = i + 1; j < size; j++) {
-                second = freqItemsets.get(j);
-                if (isQualified(first, second)) {
-                    cand = new ArrayList<Integer>();
-                    for (int tmp : first)
-                        cand.add(tmp);
-                    cand.add(second.get(second.size() - 1));
-                    candidates.add(cand);
-                } else {
-                    break;
-                }
-            }
-        }
-        return candidates;
-    }
+	/**
+	 * Self join a list of itemsets. Only join two list if and only if their
+	 * items are equals except for last one
+	 * 
+	 * @param freqItemsets
+	 * @return
+	 */
+	private List<List<Integer>> selfJoinFreqItemsets(
+			List<List<Integer>> freqItemsets) {
+		List<List<Integer>> candidates = new ArrayList<List<Integer>>();
+		int size = freqItemsets.size();
+		List<Integer> first;
+		List<Integer> second;
+		List<Integer> cand;
+		for (int i = 0; i < size; i++) {
+			first = freqItemsets.get(i);
+			for (int j = i + 1; j < size; j++) {
+				second = freqItemsets.get(j);
+				if (isQualified(first, second)) {
+					cand = new ArrayList<Integer>();
+					for (int tmp : first)
+						cand.add(tmp);
+					cand.add(second.get(second.size() - 1));
+					candidates.add(cand);
+				} else {
+					break;
+				}
+			}
+		}
+		return candidates;
+	}
 
-    /**
-     * Load K-1 frequent itemsets from folder
-     *
-     * @param folderName
-     * @return
-     * @throws IOException
-     */
-    private List<List<Integer>> loadFreqItems(String folderName,Configuration conf) throws Exception {
-        List<List<Integer>> freqItems = new ArrayList<List<Integer>>();
-        List<String> contents = utils.read(folderName, conf);
-        String[] items;
-        for(String line : contents){
-            List<Integer> candidate = new ArrayList<Integer>();
-            items = line.split("\t");
-            for (String item : items) {
-                candidate.add(Integer.parseInt(item));
-            }
-            freqItems.add(candidate);
-        }
-        return freqItems;
-    }
+	/**
+	 * Load K-1 frequent itemsets from folder
+	 *
+	 * @param folderName
+	 * @return
+	 * @throws IOException
+	 */
+	private List<List<Integer>> loadFreqItems(String folderName,
+			Configuration conf) throws Exception {
+		List<List<Integer>> freqItems = new ArrayList<List<Integer>>();
+		List<String> contents = utils.read(folderName, conf);
+		String[] items;
+		for (String line : contents) {
+			List<Integer> candidate = new ArrayList<Integer>();
+			items = line.split("\t");
+			for (String item : items) {
+				candidate.add(Integer.parseInt(item));
+			}
+			freqItems.add(candidate);
+		}
+		return freqItems;
+	}
 
-    /**
-     * Load 1-frequent itemsets from folder
-     * @param folder
-     * @return conf
-     * @throws IOException
-     */
-    private List<Integer> loadOneFreqItems(String folder, Configuration conf) throws Exception {
-        List<Integer> oneFreqItems = new ArrayList<Integer>();
-        List<String> contents = utils.read(folder, conf);
-        for (String line : contents) {
-            oneFreqItems.add(Integer.parseInt(line));
-        }
-        Collections.sort(oneFreqItems);
-        return oneFreqItems;
-    }
+	/**
+	 * Load 1-frequent itemsets from folder
+	 * 
+	 * @param folder
+	 * @return conf
+	 * @throws IOException
+	 */
+	private List<Integer> loadOneFreqItems(String folder, Configuration conf)
+			throws Exception {
+		List<Integer> oneFreqItems = new ArrayList<Integer>();
+		List<String> contents = utils.read(folder, conf);
+		for (String line : contents) {
+			oneFreqItems.add(Integer.parseInt(line));
+		}
+		Collections.sort(oneFreqItems);
+		return oneFreqItems;
+	}
 }
--- FindFreqItemsets/src/millionsongs/CombineTask.java
+++ FindFreqItemsets/src/millionsongs/CombineTask.java
@@ -17,142 +17,159 @@ import org.apache.hadoop.util.GenericOptionsParser;
 import java.io.IOException;
 
 /**
- * This is a MapReduce job aiming to emit transaction (User, song1, song2, song3 ... songn). Song ids are in asc order.
+ * This is a MapReduce job aiming to emit transaction (User, song1, song2, song3
+ * ... songn). Song ids are in asc order. <br>
  * Created by peilicao on 11/9/14.
  */
 public class CombineTask {
 	/**
-	 * This mapper will read the triplet file and emits (user,song) as output key, output value is null
+	 * This mapper will read the triplet file and emits (user,song) as output
+	 * key, output value is null
+	 * 
 	 * @author peilicao
 	 *
 	 */
-    public static class MyMapper extends Mapper<Object, Text, TupleKey, NullWritable> {
+	public static class MyMapper extends
+			Mapper<Object, Text, TupleKey, NullWritable> {
 
-        private IntWritable user;
-        private IntWritable song;
-        private TupleKey outKey;
+		private IntWritable user;
+		private IntWritable song;
+		private TupleKey outKey;
 
-        protected void setup(Context context){
-            user = new IntWritable();
-            song = new IntWritable();
-            outKey = new TupleKey();
-        }
+		protected void setup(Context context) {
+			user = new IntWritable();
+			song = new IntWritable();
+			outKey = new TupleKey();
+		}
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] items = value.toString().split("\t");
-            /**
-             * Convert userid and songid to integer for easier ordered by reducer
-             */
-            user.set(Integer.parseInt(items[0]));
-            song.set(Integer.parseInt(items[1]));
-            outKey.set(user,song);
-            context.write(outKey,NullWritable.get());
-        }
-    }
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String[] items = value.toString().split("\t");
+			/**
+			 * Convert userid and songid to integer for easier ordered by
+			 * reducer
+			 */
+			user.set(Integer.parseInt(items[0]));
+			song.set(Integer.parseInt(items[1]));
+			outKey.set(user, song);
+			context.write(outKey, NullWritable.get());
+		}
+	}
 
-    /**
-     * This reducer will read ((user,song),null) emitted by mapper and outputs transactions (User, song1, song2, song3 ... songn).
-     * The order of input key is first ordered by userid asc, then for same userid, ordered by songid asc.
-     * By doing this, we make sure that the output of reducer is in order in each line.
-     * @author peilicao
-     *
-     */
-    public static class MyReducer extends Reducer<TupleKey,NullWritable,NullWritable,Text> {
-        private static final NullWritable nullKey = NullWritable.get();
-        private Text out = new Text();
-        private StringBuilder builder = new StringBuilder();
+	/**
+	 * This reducer will read ((user,song),null) emitted by mapper and outputs
+	 * transactions (User, song1, song2, song3 ... songn). The order of input
+	 * key is first ordered by userid asc, then for same userid, ordered by
+	 * songid asc. By doing this, we make sure that the output of reducer is in
+	 * order in each line.
+	 * 
+	 * @author peilicao
+	 *
+	 */
+	public static class MyReducer extends
+			Reducer<TupleKey, NullWritable, NullWritable, Text> {
+		private static final NullWritable nullKey = NullWritable.get();
+		private Text out = new Text();
+		private StringBuilder builder = new StringBuilder();
 
-        @Override
-        public void reduce(TupleKey key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
-            builder.setLength(0);
-            builder.append(key.getFirst().get());
+		@Override
+		public void reduce(TupleKey key, Iterable<NullWritable> values,
+				Context context) throws IOException, InterruptedException {
+			builder.setLength(0);
+			builder.append(key.getFirst().get());
 
-            for(NullWritable val : values){
-                builder.append("\t");
-                builder.append(key.getSecond().get());
-            }
+			for (NullWritable val : values) {
+				builder.append("\t");
+				builder.append(key.getSecond().get());
+			}
 
-            out.set(builder.toString());
-            context.write(nullKey,out);
-        }
-    }
+			out.set(builder.toString());
+			context.write(nullKey, out);
+		}
+	}
 
-    public static class CustomizeSortComparator extends  WritableComparator{
-        protected CustomizeSortComparator() {
-            super(TagKey.class,true);
-        }
+	public static class CustomizeSortComparator extends WritableComparator {
+		protected CustomizeSortComparator() {
+			super(TagKey.class, true);
+		}
 
-        @Override
-        public int compare(WritableComparable oo1,WritableComparable oo2){
-            TagKey tk1 = (TagKey) oo1;
-            TagKey tk2 = (TagKey) oo2;
-            return tk1.compareTo(tk2);
-        }
-    }
+		@Override
+		public int compare(WritableComparable oo1, WritableComparable oo2) {
+			TagKey tk1 = (TagKey) oo1;
+			TagKey tk2 = (TagKey) oo2;
+			return tk1.compareTo(tk2);
+		}
+	}
 
-    /**
-     * Partitioning records only consider userId.
-     * @author peilicao
-     *
-     */
-    public static class CustomizePartitioner extends Partitioner<TupleKey,NullWritable>{
+	/**
+	 * Partitioning records only consider userId.
+	 * 
+	 * @author peilicao
+	 *
+	 */
+	public static class CustomizePartitioner extends
+			Partitioner<TupleKey, NullWritable> {
 
-        @Override
-        public int getPartition(TupleKey tupleKey, NullWritable intWritable, int numPartitioner) {
-            return tupleKey.getFirst().hashCode()*127 % numPartitioner;
-        }
-    }
+		@Override
+		public int getPartition(TupleKey tupleKey, NullWritable intWritable,
+				int numPartitioner) {
+			return tupleKey.getFirst().hashCode() * 127 % numPartitioner;
+		}
+	}
 
-    /**
-     * Grouping records only consider userId.
-     * @author peilicao
-     *
-     */
-    public static class CustomizeGroupComparator extends  WritableComparator{
-        protected CustomizeGroupComparator() {
-            super(TupleKey.class,true);
-        }
+	/**
+	 * Grouping records only consider userId.
+	 * 
+	 * @author peilicao
+	 *
+	 */
+	public static class CustomizeGroupComparator extends WritableComparator {
+		protected CustomizeGroupComparator() {
+			super(TupleKey.class, true);
+		}
 
-        @Override
-        public int compare(WritableComparable oo1,WritableComparable oo2){
-            TupleKey tk1 = (TupleKey) oo1;
-            TupleKey tk2 = (TupleKey) oo2;
-            return tk1.keyCompareTo(tk2);
-        }
-    }
-    
-    public static boolean run(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        Configuration conf = new Configuration();
-        String[] otherArgs = new GenericOptionsParser(conf, args)
-                .getRemainingArgs();
+		@Override
+		public int compare(WritableComparable oo1, WritableComparable oo2) {
+			TupleKey tk1 = (TupleKey) oo1;
+			TupleKey tk2 = (TupleKey) oo2;
+			return tk1.keyCompareTo(tk2);
+		}
+	}
 
-        if (otherArgs.length != 2) {
-            System.err.println("Usage: Combine <in> <out>");
-            System.exit(2);
-        }
-        Job job = new Job(conf, "Combine");
-        job.setJarByClass(CombineTask.class);
+	public static boolean run(String[] args) throws IOException,
+			ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
 
-        job.setMapperClass(MyMapper.class);
-        job.setReducerClass(MyReducer.class);
-        job.setGroupingComparatorClass(CustomizeGroupComparator.class);
-        job.setPartitionerClass(CustomizePartitioner.class);
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: Combine <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "Combine");
+		job.setJarByClass(CombineTask.class);
 
-        job.setMapOutputKeyClass(TupleKey.class);
-        job.setMapOutputValueClass(NullWritable.class);
-        job.setOutputKeyClass(NullWritable.class);
-        job.setOutputValueClass(Text.class);
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
+		job.setGroupingComparatorClass(CustomizeGroupComparator.class);
+		job.setPartitionerClass(CustomizePartitioner.class);
 
-        job.setNumReduceTasks(10);
+		job.setMapOutputKeyClass(TupleKey.class);
+		job.setMapOutputValueClass(NullWritable.class);
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(Text.class);
 
-        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		job.setNumReduceTasks(10);
 
-        return job.waitForCompletion(true);
-    }
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        boolean code = run(args);
-        System.exit(code ? 0 : 1);
-    }
+		return job.waitForCompletion(true);
+	}
+
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException, InterruptedException {
+		boolean code = run(args);
+		System.exit(code ? 0 : 1);
+	}
 }
